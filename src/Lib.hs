d1a85cfb2eb536c48feba93e3316c5a76c7626e1
Unknown <tom.bourret@insa-lyon.fr>
2018-09-27 17:43:46 +0100

    First unit tests and test tree implemented

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,5 +1,5 @@
 module Lib
-    ( someFunc, Tree, emptyTree, lca
+    ( someFunc, Tree, emptyTree, testTree, lca
     ) where
 
 someFunc :: IO ()
@@ -9,6 +9,17 @@ data Tree a = Empty | Node a (Tree a) (Tree a)
 
 emptyTree :: Tree Int
 emptyTree = Empty
+
+testTree :: Tree Int
+testTree = Node 1
+  (Node 2
+    (Node 4 Empty Empty)
+    (Node 5
+      (Node 8 Empty Empty)
+      (Node 9 Empty Empty)))
+  (Node 3
+    (Node 6 Empty Empty)
+    (Node 7 Empty Empty))
 
 lca :: Tree a -> a -> a -> Either Bool a
 lca Empty _ _ = Left False
--- test/LibSpec.hs
+++ test/LibSpec.hs
@@ -11,3 +11,15 @@ spec = do
 
     it "returns Left False for empty tree" $ do
       (lca emptyTree 1 2) `shouldBe` Left False
+
+    it "returns 1 for lca 1 1" $ do
+      (lca testTree 1 1) `shouldBe` Right 1
+
+    it "returns 1 for lca 2 3" $ do
+          (lca testTree 2 3) `shouldBe` Right 1
+
+    it "returns 2 for lca 4 9" $ do
+      (lca testTree 4 9) `shouldBe` Right 2
+
+    it "returns 2 for lca 4 100" $ do
+      (lca testTree 4 100) `shouldBe` Left False
