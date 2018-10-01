c61908a36fc3e05c59c312015f8feb01aebebc3d
Unknown <tom.bourret@insa-lyon.fr>
2018-10-01 18:53:06 +0100

    First implementation of LCA (some unit tests still fail)
    Just one of the unit tests added currently pass

--- src/Lib.hs
+++ src/Lib.hs
@@ -21,5 +21,17 @@ testTree = Node 1
     (Node 6 Empty Empty)
     (Node 7 Empty Empty))
 
-lca :: Tree a -> a -> a -> Either Bool a
+lca :: Eq a => Tree a -> a -> a -> Either Bool a
 lca Empty _ _ = Left False
+lca (Node v tl tr) n1 n2 =
+  let l = lca tl n1 n2
+      r = lca tr n1 n2
+      root = (v == n1) || (v == n2)
+  in case (l, r, root) of
+    (Left True, Left True, _) -> Right v
+    (Left True, _, True) -> Right v
+    (_, Left True, True) -> Right v
+    (Left True, _, False) -> Left True
+    (_, Left True, False) -> Left True
+    (_, _, True) -> Left True
+    _ -> Left False
