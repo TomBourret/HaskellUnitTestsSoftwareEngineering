e05c123c7be85d97e25f5c9ae071332aad605c2b
Unknown <tom.bourret@insa-lyon.fr>
2018-10-02 11:05:38 +0100

    The lca of the same node is the node

--- src/Lib.hs
+++ src/Lib.hs
@@ -27,13 +27,15 @@ lca (Node v tl tr) n1 n2 =
   let l = lca tl n1 n2
       r = lca tr n1 n2
       root = (v == n1) || (v == n2)
-  in case (l, r, root) of
-    (Right a, _, _) -> Right a
-    (_, Right a, _) -> Right a
-    (Left True, Left True, _) -> Right v
-    (Left True, _, True) -> Right v
-    (_, Left True, True) -> Right v
-    (Left True, _, False) -> Left True
-    (_, Left True, False) -> Left True
-    (_, _, True) -> Left True
+      sameNode = (n1 == n2)
+  in case (l, r, root, sameNode) of
+    (_, _, _, True) -> Right n1
+    (Right a, _, _, _) -> Right a
+    (_, Right a, _, _) -> Right a
+    (Left True, Left True, _, _) -> Right v
+    (Left True, _, True, _) -> Right v
+    (_, Left True, True, _) -> Right v
+    (Left True, _, False, _) -> Left True
+    (_, Left True, False, _) -> Left True
+    (_, _, True, _) -> Left True
     _ -> Left False
