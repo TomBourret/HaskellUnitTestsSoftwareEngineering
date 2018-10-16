c540b3f172e9634f1f85450dd7ed0d4a2fff2b61
Unknown <tom.bourret@insa-lyon.fr>
2018-10-16 14:00:19 +0100

    First steps with paths (not working yet)

--- src/Lib.hs
+++ src/Lib.hs
@@ -7,6 +7,10 @@ someFunc = putStrLn "someFunc"
 
 data Tree a = Empty | Node a (Tree a) (Tree a)
 
+type Path = [Int]
+
+type Graph = [Path]
+
 emptyTree :: Tree Int
 emptyTree = Empty
 
@@ -20,6 +24,24 @@ testTree = Node 1
   (Node 3
     (Node 6 Empty Empty)
     (Node 7 Empty Empty))
+
+path1 :: Path
+path1 = [1,2,4]
+
+path2 :: Path
+path2 = [1,2,5,9]
+
+pathToTree :: Path -> Path -> Tree Int
+pathToTree [] [] = Empty
+pathToTree p1 p2 =
+  let len1 = length p1
+      len2 = length p2
+  in case (len1 >= len2) of
+    (True) -> pathToTreeImp (drop (len1 - len2) p1) p2
+    (False) -> pathToTreeImp p1 (drop (len2 - len1) p2)
+
+pathToTreeImp :: Path -> Path -> Tree Int
+pathToTreeImp p1 p2 = (Node 1 Empty Empty)
 
 lca :: Eq a => Tree a -> a -> a -> Either Bool a
 lca Empty _ _ = Left False
