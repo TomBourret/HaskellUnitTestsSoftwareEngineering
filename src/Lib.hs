90ef8a0141f8f7ec3bd0c95af8b1c56ecf41f965
Unknown <tom.bourret@insa-lyon.fr>
2018-10-18 16:48:42 +0100

    Total changement of data structure (unit tests not modified yet)

--- src/Lib.hs
+++ src/Lib.hs
@@ -7,10 +7,6 @@ someFunc = putStrLn "someFunc"
 
 data Tree a = Empty | Node a (Tree a) (Tree a)
 
-type Path = [Int]
-
-type Graph = [Path]
-
 emptyTree :: Tree Int
 emptyTree = Empty
 
@@ -25,39 +21,25 @@ testTree = Node 1
     (Node 6 Empty Empty)
     (Node 7 Empty Empty))
 
+type Id = Int
+
+data Path = [Id] :# !Int deriving (Eq, Show)
+
+empty :: Path
+empty = [] :# 0
+
+cons :: Id -> Path -> Path
+cons a (ys :# n) = (a:ys) :# (n + 1)
+
 path1 :: Path
-path1 = [1,2,4]
+path1 = [1,2,4] :# 3
 
 path2 :: Path
-path2 = [1,2,5,9]
+path2 = [1,2,5,9] :# 4
 
-pathToTree :: Path -> Path -> Tree Int
-pathToTree [] [] = Empty
-pathToTree p1 p2 =
-  let len1 = length p1
-      len2 = length p2
-  in case (len1 >= len2) of
-    (True) -> pathToTreeImp (drop (len1 - len2) p1) p2
-    (False) -> pathToTreeImp p1 (drop (len2 - len1) p2)
-
-pathToTreeImp :: Path -> Path -> Tree Int
-pathToTreeImp p1 p2 = (Node 1 Empty Empty)
-
-lca :: Eq a => Tree a -> a -> a -> Either Bool a
-lca Empty _ _ = Left False
-lca (Node v tl tr) n1 n2 =
-  let l = lca tl n1 n2
-      r = lca tr n1 n2
-      root = (v == n1) || (v == n2)
-      sameNode = (n1 == n2)
-  in case (l, r, root, sameNode) of
-    (_          , _         , _     , True) -> Right n1
-    (Right a    , _         , _     , _)    -> Right a
-    (_          , Right a   , _     , _)    -> Right a
-    (Left True  , Left True , _     , _)    -> Right v
-    (Left True  , _         , True  , _)    -> Right v
-    (_          , Left True , True  , _)    -> Right v
-    (Left True  , _         , False , _)    -> Left True
-    (_          , Left True , False , _)    -> Left True
-    (_          , _         , True  , _)    -> Left True
-    _ -> Left False
+lca :: Path -> Path -> Path
+lca (xs0 :# i) (ys0 :# j) = go k (drop (i-k) xs0) (drop (j-k) ys0) where 
+  k = min i j
+  go n xxs@(x:xs) (y:ys) 
+    | x == y   = xxs :# n
+    | otherwise = go (n - 1) xs ys
